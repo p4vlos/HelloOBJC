784a62bbe82a9051d83d99f3425468e91ac904dd
p4vlos <nicolaou_paul@hotmail.com>
2016-09-15 19:29:53 +0300

    Initial Commit

---
+++ HelloOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOBJC
+//
+//  Created by Pavlos Nicolaou on 15/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloOBJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOBJC
+//
+//  Created by Pavlos Nicolaou on 15/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
